ef6adb3cea067cb40cf42a7fec729a4b35e69d54
Gianluca <gianlucarighini97@gmail.com>
2024-01-30 00:12:19 +0100

    controller service repository for adding new question

--- src/main/java/com/example/quizapp/controller/QuestionController.java
+++ src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -3,10 +3,7 @@ package com.example.quizapp.controller;
 import com.example.quizapp.Question;
 import com.example.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,10 +16,13 @@ public class QuestionController {
     public List<Question> getAllQuestions() {
         return questionService.getAllQuestions();
     }
-
     @GetMapping("category/{category}")
     public List<Question> getQuestionByCategory(@PathVariable String category) {
         System.out.println(category);
         return questionService.getQuestionsByCategory(category);
+    }
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
     }
 }
