00c9e92256b3f24686a7821cb5ff65b064780d7f
Gianluca <gianlucarighini97@gmail.com>
2024-01-29 22:46:01 +0100

    refactorin folder + controller layer + service layer

---
+++ src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -0,0 +1,18 @@
+package com.example.quizapp.controller;
+
+import com.example.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+    @Autowired
+    QuestionService questionService;
+    @GetMapping("allQuestions")
+    public String getAllQuestions() {
+        return questionService.getAllQuestions();
+    }
+}
